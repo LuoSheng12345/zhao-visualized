ea9d95de041d4dff2fc07be55a0553aa1618b3b5
Luo Sheng <luosheng12345abcde@gmail.com>
2021-06-25 22:45:06 +1000

    first commit

---
+++ zhao-nuxt/pages/about.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex flex-col max-w-2xl m-auto">
+        <h1>关于</h1>
+        <div>
+            <p>这个项目是基于编程随想的太子党关系网络的视觉化。去这儿看编成随想的Repo: <a href="https://github.com/programthink/zhao" target="_blank">github.com/programthink/zhao</a></p>
+            <p>这里衷心希望编程随想啥事儿没有</p>
+            <p>这个项目可以说是建党百年的贺礼</p>
+            <p>俺花了一天时间搞定了这个项目</p>
+            <p>俺的Github页面: <a href="https://github.com/LuoSheng12345" target="_blank">github.com/LuoSheng12345</a></p>
+        </div>
+        <h1>这网站咋用</h1>
+        <div>
+            <p>
+                上面可以选展示哪些个家族
+            </p>
+            <p>
+                点圆圈或者点线（不是特别好使）可以看详情
+            </p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+h1{
+    @apply text-2xl font-bold
+}
+p{
+    @apply my-2
+}
+</style>
